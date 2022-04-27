f55745f2e665fb6cc2589e8de454d34b7a5f4c21
uday <uday,acharya@expressstores.in>
2022-04-27 23:23:15 +0530

    semantic-ui added

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,10 +1,39 @@
 import React, {useState, useEffect} from 'react';
+import {Card, Button} from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 const campaignIndex =  (props) =>{ 
 
+    const renderCampaigns = ()=> {
+        const items = props.campaigns.map(address => {
+            return{
+                header: address,
+                description: <a>View campaign</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items}/>
+    }
+
     return (
-        <h1>{props.campaigns[0]}</h1>
+        <div>
+            <link
+    async
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+  />
+  <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+
+            <h3>Open Campaigns</h3>
+
+            {renderCampaigns()}
+            <Button content="Create Campaign" 
+                    icon="add circle"
+                    primary  // Boolean props can be sent directly like this
+                    />
+        </div>
+        
     )
 };
 
